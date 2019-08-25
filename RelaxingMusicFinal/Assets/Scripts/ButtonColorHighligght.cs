345c583e0998767c2caefa31102dce1eea0c65ad
Jeffrey <jepoygimo@gmail.com>
2019-08-24 20:20:05 -0600

    Started Final Project

---
+++ RelaxingMusicFinal/Assets/Scripts/ButtonColorHighligght.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColorHighligght : MonoBehaviour
+{
+    Button btn;
+    Image color;
+    AudioSource MusicSource;
+    //float speed = 0.1f;
+    RectTransform btnSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        btn = gameObject.GetComponent<Button>();
+        color = gameObject.GetComponent<Image>();
+        MusicSource = gameObject.GetComponent<AudioSource>();
+        MusicSource.clip = Resources.Load<AudioClip>(gameObject.name);
+        //btnSize.gameObject.GetComponent<RectTransform>().anchorMax = new Vector2(1.25f, 1.25f);
+        
+
+        btn.onClick.AddListener(PausePlayMusic);
+
+    }
+
+    void PausePlayMusic()
+    {
+        if (!MusicSource.isPlaying)
+        {
+            MusicSource.Play();
+            color.color = Color.cyan;
+            
+        }
+        else
+        {
+            MusicSource.Pause();
+            color.color = Color.white;
+        }
+    }
+
+    
+}
---
+++ RelaxingMusicFinal/Assets/Scripts/MoveTestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTestScript : MonoBehaviour
+{
+    float speed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        gameObject.transform.Translate(0, speed, 0);
+        if (gameObject.transform.position.z >= 5 || gameObject.transform.position.z <= -5)
+        {
+            speed = -speed;
+        }
+    }
+
+
+}
